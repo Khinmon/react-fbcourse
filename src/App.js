af7be0e683f86566e41c535940299128590768c9
Khinmon <monmyatthu2009@gmail.com>
2023-02-08 15:24:53 +0630

    state in function component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { createRef } from "react";
 import Users from "./Users";
 
-const App = (props) => (
-  <ul>
-    <Users user="Aung Gyi" age="22" />
-    <Users user="Maung Gyi" age="21" />
-  </ul>
-);
+const App = (props) => {
+  // let [user, setUser] = React.useState("Eain Kyi");
+  // let [age, setAge] = React.useState(22);
+
+  let [users, setUsers] = React.useState([
+    { id: 1, user: "Eain Kyi", age: 22 },
+    { id: 2, user: "Aung Gyi", age: 21 },
+  ]);
+
+  let userRef = createRef();
+  let ageRef = createRef();
+
+  let plus = () => {
+    let id = users.length + 1;
+    let user = userRef.current.value;
+    let age = ageRef.current.value;
+    setUsers([...users, { id, user, age }]);
+  };
+
+  return (
+    <div>
+      <br />
+      <input type="text" ref={userRef} />
+      <br />
+      <input type="text" ref={ageRef} />
+      <br />
+      <button onClick={plus}>Plus +</button>
+      <ul>
+        {users.map((u) => (
+          <Users key={u.id} user={u.user} age={u.age} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
